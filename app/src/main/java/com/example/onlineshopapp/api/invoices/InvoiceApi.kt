51a449bc246d73644d6f905c8e131f17f585b797
AliShiri <Your new email>
2023-11-19 11:55:16 -0800

    edit invoiceApi

--- app/src/main/java/com/example/onlineshopapp/api/invoices/InvoiceApi.kt
+++ app/src/main/java/com/example/onlineshopapp/api/invoices/InvoiceApi.kt
@@ -5,6 +5,7 @@ import com.example.onlineshopapp.models.invoices.Invoice
 import com.example.onlineshopapp.models.products.ProductColor
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -12,16 +13,23 @@ import retrofit2.http.Query
 interface InvoiceApi {
 
     @POST("/api/invoice/")
-    suspend fun addInvoice(@Body invoice: Invoice): ServiceResponse<Invoice>
+    suspend fun addInvoice(
+        @Body invoice: Invoice,
+        @Header("Authorization") token: String,
+    ): ServiceResponse<Invoice>
 
     @GET("/api/invoice/{id}")
-    suspend fun getInvoiceById(@Path("id") id: Long): ServiceResponse<Invoice>
+    suspend fun getInvoiceById(
+        @Path("id") id: Long,
+        @Header("Authorization") token: String,
+    ): ServiceResponse<Invoice>
 
     @GET("/api/invoice/user/{userid}")
     suspend fun getInvoiceByUserId(
         @Path("userid") id: Long,
         @Query("pageIndex") pageIndex: Int,
         @Path("pageSize") pageSize: Int,
+        @Header("Authorization") token: String,
     ): ServiceResponse<Invoice>
 
 
