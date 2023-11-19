b01bc2d3ebe12270ff7e26508b1f05942cd58f0a
AliShiri <Your new email>
2023-11-19 12:01:26 -0800

    add addProductCategoryApi

---
+++ app/src/main/java/com/example/onlineshopapp/api/products/ProductCategoryApi.kt
@@ -0,0 +1,16 @@
+package com.example.onlineshopapp.api.products
+
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.products.ProductCategory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ProductCategoryApi {
+
+    @GET("/api/productCategory")
+    suspend fun getColor(): ServiceResponse<ProductCategory>
+
+    @GET("/api/productCategory/{id}")
+    suspend fun getColorById(@Path("id") id: Long): ServiceResponse<ProductCategory>
+
+}
