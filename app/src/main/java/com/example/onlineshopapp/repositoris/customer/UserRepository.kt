0e14fe3f4e170c1d11f79635dc6a6a5185bb6752
AliShiri <Your new email>
2023-11-22 08:03:03 -0800

    complete userRepository and provide it in appModule

--- app/src/main/java/com/example/onlineshopapp/repositoris/customer/UserRepository.kt
+++ app/src/main/java/com/example/onlineshopapp/repositoris/customer/UserRepository.kt
@@ -1,9 +1,68 @@
 package com.example.onlineshopapp.repositoris.customer
 
 import com.example.onlineshopapp.api.customer.UserApi
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.customer.User
+import com.example.onlineshopapp.models.customer.UserVM
+import com.example.onlineshopapp.models.invoices.Invoice
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @ActivityScoped
 class UserRepository @Inject constructor(private val userApi: UserApi) {
+
+    suspend fun getUserInfo(token: String): ServiceResponse<User> {
+
+        return try {
+            userApi.getUserInfo(token)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun getBlogById(
+        user: UserVM,
+        token: String,
+    ): ServiceResponse<User> {
+
+        return try {
+            userApi.getBlogById(user, token)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun login(
+        user: UserVM,
+    ): ServiceResponse<UserVM> {
+
+        return try {
+            userApi.login(user)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun register(
+        user: UserVM,
+    ): ServiceResponse<User> {
+
+        return try {
+            userApi.register(user)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun update(
+        user: UserVM,
+        token: String,
+    ): ServiceResponse<User> {
+
+        return try {
+            userApi.update(user, token)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
 }
