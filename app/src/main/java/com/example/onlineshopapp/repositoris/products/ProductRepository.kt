7e9b9e280b8a930726533337610a5c4ecd50b65b
AliShiri <Your new email>
2023-11-22 06:42:32 -0800

    complete ProductRepository and provide  it in AppModule

--- app/src/main/java/com/example/onlineshopapp/repositoris/products/ProductRepository.kt
+++ app/src/main/java/com/example/onlineshopapp/repositoris/products/ProductRepository.kt
@@ -1,9 +1,51 @@
 package com.example.onlineshopapp.repositoris.products
 
 import com.example.onlineshopapp.api.products.ProductApi
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.products.Product
+import com.example.onlineshopapp.models.products.ProductCategory
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @ActivityScoped
 class ProductRepository @Inject constructor(private val productApi: ProductApi) {
+
+
+    suspend fun getProduct(): ServiceResponse<Product> {
+
+        return try {
+            productApi.getProduct()
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun getProductById(id: Long): ServiceResponse<Product> {
+
+        return try {
+            productApi.getProductById(id)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun getNewProduct(): ServiceResponse<Product> {
+
+        return try {
+            productApi.getNewProduct()
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun getPopularProduct(): ServiceResponse<Product> {
+
+        return try {
+            productApi.getPopularProduct()
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+
 }
