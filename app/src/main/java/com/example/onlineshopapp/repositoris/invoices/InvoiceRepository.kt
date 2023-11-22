ad2998fad30b96aa577d223c1de3c68539ee4ad9
AliShiri <Your new email>
2023-11-22 07:38:38 -0800

    complete InvoiceRepository and provide it in appModule

--- app/src/main/java/com/example/onlineshopapp/repositoris/invoices/InvoiceRepository.kt
+++ app/src/main/java/com/example/onlineshopapp/repositoris/invoices/InvoiceRepository.kt
@@ -1,9 +1,46 @@
 package com.example.onlineshopapp.repositoris.invoices
 
 import com.example.onlineshopapp.api.invoices.InvoiceApi
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.invoices.Invoice
+import com.example.onlineshopapp.models.products.ProductColor
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @ActivityScoped
 class InvoiceRepository @Inject constructor(private val invoiceApi: InvoiceApi) {
+
+    suspend fun addInvoice(invoice: Invoice, token: String): ServiceResponse<Invoice> {
+
+        return try {
+            invoiceApi.addInvoice(invoice, token)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun getInvoiceById(id: Long, token: String): ServiceResponse<Invoice> {
+
+        return try {
+            invoiceApi.getInvoiceById(id, token)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun getInvoiceByUserId(
+        id: Long,
+        pageIndex: Int,
+        pageSize: Int,
+        token: String,
+    ): ServiceResponse<Invoice> {
+
+        return try {
+            invoiceApi.getInvoiceByUserId(id, pageIndex, pageSize, token)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+
 }
