2fbadd272ac528a82c5202fd8eb08e16485fb450
AliShiri <Your new email>
2023-11-22 10:57:42 -0800

    create contentViewModel and complete it

---
+++ app/src/main/java/com/example/onlineshopapp/viewModels/products/ContentViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.onlineshopapp.viewModels.products
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.site.Content
+import com.example.onlineshopapp.repositoris.site.ContentRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ContentViewModel @Inject constructor(private val contentRepository: ContentRepository) :
+    ViewModel() {
+
+    fun getContent(onResponse: (response: ServiceResponse<Content>) -> Unit) {
+        viewModelScope.launch {
+            var response = contentRepository.getContent()
+
+            onResponse(response)
+        }
+    }
+
+    fun getContentByTitle(title: String, onResponse: (response: ServiceResponse<Content>) -> Unit) {
+        viewModelScope.launch {
+            var response = contentRepository.getContentByTitle(title)
+
+            onResponse(response)
+        }
+    }
+
+}
