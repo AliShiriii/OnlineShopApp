d372e7213872268891084cf4c07d94aad752b6d3
AliShiri <Your new email>
2023-11-22 07:15:14 -0800

    complete transactionRepository and provide it in appModule

--- app/src/main/java/com/example/onlineshopapp/repositoris/invoices/TransactionRepository.kt
+++ app/src/main/java/com/example/onlineshopapp/repositoris/invoices/TransactionRepository.kt
@@ -1,9 +1,23 @@
 package com.example.onlineshopapp.repositoris.invoices
 
 import com.example.onlineshopapp.api.invoices.TransactionApi
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.invoices.PaymentTransaction
+import com.example.onlineshopapp.models.invoices.Transaction
+import com.example.onlineshopapp.models.products.ProductColor
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @ActivityScoped
 class TransactionRepository @Inject constructor(private val transactionApi: TransactionApi) {
+
+
+    suspend fun goToPayment(data: PaymentTransaction): ServiceResponse<String> {
+
+        return try {
+            transactionApi.goToPayment(data)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
 }
