939774ec34d880ebdf51b0de6d3efd793b6c3ff8
AliShiri <Your new email>
2023-11-22 11:21:46 -0800

    create  blogViewModel and complete it

---
+++ app/src/main/java/com/example/onlineshopapp/viewModels/products/BlogViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.onlineshopapp.viewModels.products
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.site.Blog
+import com.example.onlineshopapp.models.site.Content
+import com.example.onlineshopapp.repositoris.site.BlogRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BlogViewModel @Inject constructor(private val blogRepository: BlogRepository) : ViewModel() {
+
+
+    fun getBlog(onResponse: (response: ServiceResponse<Blog>) -> Unit) {
+        viewModelScope.launch {
+            var response = blogRepository.getBlog()
+
+            onResponse(response)
+        }
+    }
+
+    fun getBlogById(id: Long, onResponse: (response: ServiceResponse<Blog>) -> Unit) {
+        viewModelScope.launch {
+            var response = blogRepository.getBlogById(id)
+
+            onResponse(response)
+        }
+    }
+
+}
