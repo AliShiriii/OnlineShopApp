25ce227f3f2fe2a2a41d0162cbd6ab0a399db7dd
AliShiri <Your new email>
2023-11-22 06:54:06 -0800

    complete colorRepository and provide it in appModule

--- app/src/main/java/com/example/onlineshopapp/repositoris/products/ColorRepository.kt
+++ app/src/main/java/com/example/onlineshopapp/repositoris/products/ColorRepository.kt
@@ -1,9 +1,29 @@
 package com.example.onlineshopapp.repositoris.products
 
 import com.example.onlineshopapp.api.products.ColorApi
+import com.example.onlineshopapp.models.ServiceResponse
+import com.example.onlineshopapp.models.products.ProductColor
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @ActivityScoped
 class ColorRepository @Inject constructor(private val colorApi: ColorApi) {
+
+    suspend fun getColor(): ServiceResponse<ProductColor> {
+
+        return try {
+            colorApi.getColor()
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
+
+    suspend fun getProductById(id: Long): ServiceResponse<ProductColor> {
+
+        return try {
+            colorApi.getColorById(id)
+        } catch (e: Exception) {
+            ServiceResponse(status = "EXCEPTION", message = e.message)
+        }
+    }
 }
